1c1d280c30c0a31650d72c4615da525509c95b54
GT Nunes <wisecoder01@gmail.com>
2024-06-14 21:23:22 -0300

    Release 1.1.8

--- app_main.py
+++ app_main.py
@@ -31,7 +31,7 @@ from migration import (
 )  # noqa: PyPep8
 
 # establish the current version
-APP_VERSION: Final[str] = "1.1.7"
+APP_VERSION: Final[str] = "1.1.8"
 
 # create the Flask application
 app: Flask = Flask(__name__)
