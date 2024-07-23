1bcec68f36f54f29b16cae6876cca729ce0e408d
GT Nunes <wisecoder01@gmail.com>
2024-07-23 17:28:18 -0300

    Release 1.3.1

--- migration/pydb_types.py
+++ migration/pydb_types.py
@@ -546,8 +546,8 @@ def migrate_column(source_rdbms: str,
     is_number_int: bool = (is_number and
                            hasattr(col_type_obj, "asdecimal") and
                            not col_type_obj.asdecimal)
-    col_precision: int = col_type_obj.precision \
-        if is_number and hasattr(col_type_obj, "precision") else None
+    col_precision: int = (col_type_obj.precision
+                          if is_number and hasattr(col_type_obj, "precision") else None)
 
     # PostgreSQL does not accept value '0' in 'CACHE' clause, at table creation time
     # (cannot just remove the attribute, as SQLAlchemy requires it to exist in identity columns)
