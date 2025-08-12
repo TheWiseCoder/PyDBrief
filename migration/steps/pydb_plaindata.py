69c0871749d66b11780b25cd36348270227fd550
GT Nunes <wisecoder01@gmail.com>
2025-08-11 21:35:37 -0300

    Release 1.9.1

--- migration/steps/pydb_plaindata.py
+++ migration/steps/pydb_plaindata.py
@@ -165,7 +165,7 @@ def migrate_plain(errors: list[str],
 
                     # build migration channel data ([(offset, limit),...])
                     channel_data: list[tuple[int, int]] = \
-                        build_channel_data(max_channels=session_metrics[MigMetric.PLAINDATA_CHANNELS],
+                        build_channel_data(  # max_channels=session_metrics[MigMetric.PLAINDATA_CHANNELS],
                                            channel_size=session_metrics[MigMetric.PLAINDATA_CHANNEL_SIZE],
                                            table_count=table_count,
                                            offset_count=offset_count,
