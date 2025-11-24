1ae3d7a4a40677bbf4bff09b52d54480ac0ed03c
GT Nunes <wisecoder01@gmail.com>
2025-11-24 17:52:47 +0000

    Release 2.0.5

--- app_ident.py
+++ app_ident.py
@@ -7,4 +7,4 @@ os.environ["PYDB_VALIDATION_MSG_PREFIX"] = ""
 
 # establish the app's name and current version
 APP_NAME: str = "PyDBrief"
-APP_VERSION: Final[str] = "2.0.4"
+APP_VERSION: Final[str] = "2.0.5"
