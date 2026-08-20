dd884ad242632ebb1bdf56d8539276e9d4044eba
GT Nunes <wisecoder01@gmail.com>
2026-08-20 12:28:25 -0300

    Release 2.2.1

--- app_ident.py
+++ app_ident.py
@@ -6,7 +6,7 @@ from typing import Final
 
 # establish the app's name and current version
 APP_NAME: str = "PyDBrief"
-APP_VERSION: Final[str] = "2.2.0"
+APP_VERSION: Final[str] = "2.2.1"
 
 # load environment variables for local execution
 if os.getenv("APP_LOCAL_DEPLOYMENT") == "1":
