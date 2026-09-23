246a738b3da275a6d50db434d702e54fd64160a9
GT Nunes <wisecoder01@gmail.com>
2026-09-23 16:53:02 -0300

    Release 2.3.5

--- app_ident.py
+++ app_ident.py
@@ -9,7 +9,7 @@ APP_NAME: str = "PyDBrief"
 APP_VERSION: Final[str] = "2.3.5"
 
 # load environment variables for local execution
-# (this must precede importing 'pypomes_core')
+# (this must precede importing 'pypomes-core')
 if os.getenv("APP_LOCAL_DEPLOYMENT") == "1":
     load_dotenv(dotenv_path=Path.cwd() / ".env_local")
 
@@ -60,5 +60,5 @@ def __set_logging_file_path():
 
 
 # set the logging file's path
-# (this must precede importing 'pypomes_logging')
+# (this must precede importing 'pypomes-logging')
 __set_logging_file_path()
